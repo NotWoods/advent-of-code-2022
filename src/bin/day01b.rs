7e0e5f014c61a5aea8fbd9317eb6c3caa8c46fc3
Tiger Oakes <contact@tigeroakes.com>
2022-12-05 03:33:27 +0000

    Day1b

--- src/bin/day01b.rs
+++ src/bin/day01b.rs
@@ -1,15 +1,15 @@
-use std::cmp::max;
+use std::collections::BinaryHeap;
 use std::io::stdin;
 
-// cargo run --bin day01a < input/day01.txt
+// cargo run --bin day01b < input/day01.txt
 
 fn main() {
-    let mut largest_so_far: i32 = 0;
+    let mut largest = BinaryHeap::new();
     let mut current_elf_calories: i32 = 0;
     for line in stdin().lines() {
         let input = line.unwrap();
         if input.is_empty() {
-            largest_so_far = max(largest_so_far, current_elf_calories);
+            largest.push(current_elf_calories);
             current_elf_calories = 0;
         } else {
             let calories: i32 = input.parse().expect("Input not an integer");
@@ -17,5 +17,10 @@ fn main() {
         }
     }
 
-    println!("{}", largest_so_far);
+    let mut top_n_calories_sum = 0;
+    for _ in 0..3 {
+        top_n_calories_sum += largest.pop().unwrap();
+    }
+
+    println!("{}", top_n_calories_sum);
 }
